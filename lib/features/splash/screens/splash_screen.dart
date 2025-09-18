3d8daf06e227d0da5932739aa3692166d0fc4ff1
ammaribrahim-rpl <ammar01ibrahim@gmail.com>
2025-09-18 09:11:42 +0700

    style: splash

--- lib/features/splash/screens/splash_screen.dart
+++ lib/features/splash/screens/splash_screen.dart
@@ -8,34 +8,42 @@ class SplashScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final c = Get.put(NewsController()); // ambil controller theme
+
     // Setelah 3 detik pindah ke ApiScreen
-    Future.delayed(const Duration(seconds: 3), () {
+    Future.delayed(const Duration(seconds: 5), () {
       Get.off(() => ApiScreen());
     });
 
-    return Scaffold(
-      backgroundColor: Colors.white, // bisa disesuaikan dengan dark/light mode
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            // Animasi Lottie
-            Lottie.asset(
-              'assets/animations/global_news.json',
-              width: 200,
-              height: 200,
-              fit: BoxFit.contain,
-            ),
-            const SizedBox(height: 20),
-            Text(
-              "Breaking News",
-              style: TextStyle(
-                fontSize: 26,
-                fontWeight: FontWeight.bold,
-                color: Colors.deepPurple,
+    return Obx(
+      () => Scaffold(
+        backgroundColor: c.isTheme.value
+            ? MainColors.surfaceDark
+            : MainColors.surfaceLight,
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              // Animasi Lottie
+              Lottie.asset(
+                'assets/animations/traffic.json',
+                width: 300,
+                height: 300,
+                fit: BoxFit.contain,
               ),
-            ),
-          ],
+              const SizedBox(height: 20),
+              Text(
+                "Breaking News",
+                style: TextStyle(
+                  fontSize: 26,
+                  fontWeight: FontWeight.bold,
+                  color: c.isTheme.value
+                      ? MainColors.textDark
+                      : MainColors.textLight,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
